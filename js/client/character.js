346714988d879551815e501ff8bb10d1329e377c
David <xudavidb@gmail.com>
2016-09-08 16:44:30 -0700

    extract character

---
+++ js/client/character.js
@@ -0,0 +1,57 @@
+import * as util from './utilities'
+// extends basic gob
+export default class Character extends Pew.Gob {
+  constructor(...args) {
+    super(...args)
+    let that = this
+
+    // events hash from event name to function
+    this.events = {
+      keydown: function(evt) {
+        console.log('hello')
+        switch (evt.keyCode)  {
+          case util.keys.W:
+            that.velocity = Pew.constants.VELOCITY.N
+            break
+          case util.keys.A:
+            that.velocity = Pew.constants.VELOCITY.W
+            break
+          case util.keys.S:
+            that.velocity = Pew.constants.VELOCITY.S
+            break
+          case util.keys.D:
+            that.velocity = Pew.constants.VELOCITY.E
+            break
+          default:
+            break
+        }
+      },
+      keyup: function(evt) {
+        switch (evt.keyCode)  {
+          case util.keys.W:
+          case util.keys.A:
+          case util.keys.S:
+          case util.keys.D:
+            that.velocity = {
+              x: 0,
+              y: 0
+            }
+            break;
+          default:
+            break
+        }
+      }
+    }
+  }
+
+  update() {
+    if (this.velocity.y || this.velocity.x) {
+      this.position.y -= this.velocity.y * this.speed.y;
+      this.position.x += this.velocity.x * this.speed.x;
+
+      this.data.sprite.position.y = this.position.y;
+      this.data.sprite.position.x = this.position.x;
+    }
+  }
+
+}
