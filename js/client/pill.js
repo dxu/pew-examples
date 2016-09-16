b16cf742c851135b2f80b7f1d359621adf779dc9
David <xudavidb@gmail.com>
2016-09-16 16:01:29 -0700

    stress test pixijs

--- js/client/pill.js
+++ js/client/pill.js
@@ -1,2 +1,15 @@
 // extends basic gob
-export default class Pill extends Pew.Gob {}
+export default class Pill extends Pew.Gob {
+  constructor(game, opts) {
+    opts.width = opts.width || 10
+    opts.height = opts.height || 10
+    super(game, opts)
+  }
+  onCollide(gob) {
+    // if (gob instanceof Pill) {
+    //   this.destroy()
+
+    // }
+
+  }
+}
